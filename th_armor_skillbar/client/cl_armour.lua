77d789ca738e4583264283dd19fbbdcade3fa62c
Matan Zharovsky <64105330+Matan32@users.noreply.github.com>
2020-07-22 04:23:24 +0300

    Add files via upload

--- th_armor_skillbar/client/cl_armour.lua
+++ th_armor_skillbar/client/cl_armour.lua
@@ -18,11 +18,20 @@ AddEventHandler('th_armor:armor', function()
   if GetPedArmour(GetPlayerPed(-1)) == 100 then
     TriggerEvent('notification', 'You are already wearing armor ')
   else
-    TriggerEvent('th_armor:vest')
-    exports['np-taskbar']:taskBar(5000, 'Setting Up Armor...')
-    TriggerEvent('notification', 'Succseesfully equipped an armor')
-    TriggerServerEvent('th_armor:armorremove')
-    SetPedArmour(GetPlayerPed(-1), 100)
+    local finished = exports["reload-skillbar"]:taskBar(4000,math.random(5,15))
+    if finished ~= 100 then
+        TriggerEvent('notification', 'Failed to equip armor', 2)
+    else
+      local finished2 = exports["reload-skillbar"]:taskBar(4000,math.random(5,15))
+      if finished2 ~= 100 then
+          TriggerEvent('notification', 'Failed to equip armor', 2)
+      else
+      TriggerEvent('th_armor:vest')
+      exports['np-taskbar']:taskBar(5000, 'Setting Up Armor...')
+      TriggerEvent('notification', 'Succseesfully equiped an armor')
+      SetPedArmour(GetPlayerPed(-1), 100)
+      end
+    end
   end
 end)
 
@@ -32,9 +41,3 @@ AddEventHandler("th_armor:vest", function()
     vestDrawable, vestTexture, vestPalette = GetPedDrawableVariation(playerPed, 9), GetPedTextureVariation(playerPed, 9), GetPedPaletteVariation(playerPed, 9)
     TaskPlayAnim(playerPed, "clothingshirt", "try_shirt_positive_a", 3.5, -8, -1, 49, 0, 0, 0, 0)
   end)
-
-
-
-
-
-
